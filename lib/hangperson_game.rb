0b6f1eab0269a1cad4ca36d3dafc79e3dd947e77
Ponser <kenlathan@me.com>
2015-11-07 20:59:24 +0000

    201511-7:2-18 UTC Everything seems to be working correctly.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,4 +1,6 @@
 class HangpersonGame
+  
+  # Looks good 151107:2018 UTC
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -26,26 +28,28 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
   
-  def dump
+  def dump # This is just for debugging and never should be called in a released version
     ', guesses = ' + @guesses + ', wrong_guesses = ' + @wrong_guesses
   end
   
   def guess(letter)
-    letter = letter.downcase
-    puts 'letter = ' + letter
-    if @word.include? letter
-      if @guesses.include?(letter) || @wrong_guesses.include?(letter)
-        puts 'that was a repeat'
-        return false
-      end
-      @guesses += letter
-      puts 'yes' + dump
-      return true
-    else
-      @wrong_guesses += letter
-      puts 'no' + dump
+    # /[[:alpha:]]/ matches a letter of the alphabet.
+    # See http://stackoverflow.com/questions/14551256/how-to-find-out-in-ruby-if-a-character-is-a-letter-or-a-digit
+    throw ArgumentError if !letter || (1 != letter.length) || 0 != (/[[:alpha:]]/ =~ letter)
+    
+    letter = letter.downcase # Do all work in lowercase so we don't have to check later
+    
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter) # Test for a letter already used
       return false
     end
+    
+    # The letter hasn't been used before, so it is either right or wrong.
+    if @word.include? letter
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    return true
   end
   
   def check_win_or_lose
@@ -53,10 +57,18 @@ class HangpersonGame
       return :lose
     end
     
-    if @guesses.length == @word.length
+    if word_with_guesses == word
       return :win
     end
     
     return :play
+  end
+  
+  def word_with_guesses
+    # Make a string with dashes for unguessed letters, whille showing correct guesses in place.
+    # e.g. '-ookk-----' for word 'bookkeeper' and guesses 'o' and 'k'
+    wwg = ''
+    @word.chars {|x| wwg += @guesses.include?(x) ? x : '-'}
+    return wwg
   end
 end
