b9b818c6286bffa5e55a4cbf3154c93bc77510ed
Ponser <kenlathan@me.com>
2015-11-07 18:19:58 +0000

    First test complete (create new instance)

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,12 +4,19 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
   # def initialize()
   # end
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +25,38 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  
+  def dump
+    ', guesses = ' + @guesses + ', wrong_guesses = ' + @wrong_guesses
+  end
+  
+  def guess(letter)
+    letter = letter.downcase
+    puts 'letter = ' + letter
+    if @word.include? letter
+      if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+        puts 'that was a repeat'
+        return false
+      end
+      @guesses += letter
+      puts 'yes' + dump
+      return true
+    else
+      @wrong_guesses += letter
+      puts 'no' + dump
+      return false
+    end
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+    
+    if @guesses.length == @word.length
+      return :win
+    end
+    
+    return :play
+  end
 end
